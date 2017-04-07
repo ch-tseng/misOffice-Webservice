2e53e59f677ce96d5323ee4dd56c5b4f364b01bf
ch-tseng <ch.tseng@sunplusit.com>
2017-04-07 09:10:47 +0800

    first commit

---
+++ web/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Uploading', version='1.0',
+      description='Upload image file using Flask',
+      author='CH', author_email='myvno@hotmail.com',
+      url='http://wordpress/',
+
+      #  Uncomment one or more lines below in the install_requires section
+      #  for the specific client drivers/modules your application needs.
+      install_requires=['flask']
+     )
+
